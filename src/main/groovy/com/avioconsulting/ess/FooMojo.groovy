93f1c25925ac681fd03092483ae5a57d357771fa
Brady Wied <brady@wied.us>
2017-01-19 09:52:48 -0700

    Got this approach to work by reordering/fixing generated POM

--- src/main/groovy/com/avioconsulting/ess/FooMojo.groovy
+++ src/main/groovy/com/avioconsulting/ess/FooMojo.groovy
@@ -5,16 +5,18 @@ import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugin.MojoFailureException
 import org.apache.maven.plugins.annotations.Mojo
 import weblogic.management.scripting.utils.WLSTInterpreter
+import weblogic.tools.maven.plugins.configure.WLSTClientMojo
 
 @Mojo(name='foo')
-class FooMojo extends AbstractMojo {
+class FooMojo extends WLSTClientMojo {
     void execute() throws MojoExecutionException, MojoFailureException {
         this.log.info('testing out stuff')
-        def props = [
-                (WLSTInterpreter.ENABLE_SCRIPT_MODE): Boolean.TRUE
+        def scriptLines = [
+                'connect(url=\'t3://localhost:7001\', username=\'weblogic\', password=\'oracle1234\')',
+                'domainRuntime()',
+                "manageSchedulerJobDefn('SHOW', 'EssNativeHostingApp')"
         ]
-        def interpreter = new WLSTInterpreter(props)
-        interpreter.exec('connect(url=\'t3://localhost:7001\', username=\'weblogic\', password=\'oracle1234\')')
-        interpreter.exec('disconnect()')
+        this.script = scriptLines.join '\n'
+        super.execute()
     }
 }
