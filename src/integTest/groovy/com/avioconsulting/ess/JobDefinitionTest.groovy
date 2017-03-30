0d0b35f32c9fbec01a110ea2e4b9f996682db932
Brady Wied <brady@wied.us>
2017-03-30 17:24:46 -0600

    Extract out a common base test class

--- src/integTest/groovy/com/avioconsulting/ess/JobDefinitionTest.groovy
+++ src/integTest/groovy/com/avioconsulting/ess/JobDefinitionTest.groovy
@@ -1,34 +1,15 @@
 package com.avioconsulting.ess
 
 import com.avioconsulting.ess.factories.JobDefinitionFactory
-import com.avioconsulting.ess.models.EssClientPolicySubject
-import com.avioconsulting.ess.mojos.CommonMojo
 import com.avioconsulting.ess.mojos.FieldConstants
 import com.avioconsulting.ess.mojos.JobScheduleMojo
-import com.avioconsulting.ess.wrappers.MetadataServiceWrapper
-import org.junit.Before
 import org.junit.Test
 
 import static org.hamcrest.Matchers.*
 import static org.junit.Assert.assertThat
 
 @SuppressWarnings("GroovyAccessibility")
-class IntegrationTest {
-    static final String hostname = System.getProperty('weblogic.hostname') ?: 'localhost'
-    static final String port = System.getProperty('weblogic.port') ?: '8001'
-    static final String username = System.getProperty('weblogic.username') ?: 'weblogic'
-    static final String password = System.getProperty('weblogic.password') ?: 'oracle1234'
-    private Map<Class, List<Class>> factories
-
-    static def setFields(CommonMojo mojo, Map fields, Class overrideClass = null) {
-        def klass = overrideClass ?: mojo.class
-        fields.each { key, value ->
-            def field = klass.getDeclaredField(key as String)
-            field.accessible = true
-            field.set(mojo, value)
-        }
-    }
-
+class JobDefinitionTest extends Common {
     JobScheduleMojo getMojo() {
         def mojo = new JobScheduleMojo()
         setCommonMojoFields(mojo)
@@ -42,14 +23,6 @@ class IntegrationTest {
                           serverTimeZone    : 'America/Denver'
                   ]
         mojo
-    }
-
-    @Before
-    void cleanup() {
-        this.factories = [:]
-        def mojo = getMojo()
-        mojo.cleanFirst = true
-        mojo.execute()
     }
 
     @Test
@@ -111,23 +84,5 @@ class IntegrationTest {
                    is(empty())
         assertThat mojo.canceledJobDefs,
                    is(empty())
-    }
-
-    private void mockDiscoveredFactories(CommonMojo mojo) {
-        mojo.metaClass.getSubTypesOf = { Class intf ->
-            this.factories.containsKey(intf) ? this.factories[intf] : []
-        }
-    }
-
-    private static setCommonMojoFields(CommonMojo mojo) {
-        def url = "t3://${hostname}:${port}".toString()
-        setFields mojo, [
-                soaWeblogicUrl      : url,
-                weblogicUser        : username,
-                weblogicPassword    : password,
-                configurationPackage: Factory.package.name,
-                essHostingApp       : EssClientPolicySubject.DEFAULT_ESS_HOST_APP,
-                essDeployPackage    : MetadataServiceWrapper.DEFAULT_ESS_DEPLOY_PACKAGE
-        ], CommonMojo
     }
 }
