2e1116d4340972518303bb67c4c3b5b4dea47c2b
Brady Wied <brady@wied.us>
2017-03-30 17:41:20 -0600

    Test schedule updating

--- src/integTest/groovy/com/avioconsulting/ess/ScheduleTest.groovy
+++ src/integTest/groovy/com/avioconsulting/ess/ScheduleTest.groovy
@@ -6,6 +6,7 @@ import org.junit.Test
 import static org.hamcrest.Matchers.*
 import static org.junit.Assert.assertThat
 
+@SuppressWarnings("GroovyAccessibility")
 class ScheduleTest extends Common {
     @Test
     void NoExistingSchedule_CreatesWithoutError() {
@@ -21,6 +22,26 @@ class ScheduleTest extends Common {
         assertThat mojo.newSchedules,
                    is(equalTo([expectedSchedule]))
         assertThat mojo.updatedSchedules,
+                   is(empty())
+    }
+
+    @Test
+    void ExistingSchedule_Changed_UpdatesWithoutError() {
+        // arrange
+        this.factories[ScheduleFactory] = [SingleScheduleFactory]
+        def mojo = getJobScheduleMojo()
+        mojo.execute()
+        this.factories[ScheduleFactory] = [UpdatedScheduleFactory]
+        mojo = getJobScheduleMojo()
+
+        // act
+        mojo.execute()
+
+        // assert
+        def expectedSchedule = new UpdatedScheduleFactory().createSchedule()
+        assertThat mojo.updatedSchedules,
+                   is(equalTo([expectedSchedule]))
+        assertThat mojo.newSchedules,
                    is(empty())
     }
 }
