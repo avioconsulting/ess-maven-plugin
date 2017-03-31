550eb6f68f5d33a4f3dfc8f5be3b3ea73353df82
Brady Wied <brady@wied.us>
2017-03-30 18:17:26 -0600

    Simplify schedules and put them into the future so that they are valid

--- src/integTest/groovy/com/avioconsulting/ess/UpdatedScheduleFactory.groovy
+++ src/integTest/groovy/com/avioconsulting/ess/UpdatedScheduleFactory.groovy
@@ -1,36 +1,24 @@
 package com.avioconsulting.ess
 
-import com.avioconsulting.ess.builders.ScheduleBuilder
 import com.avioconsulting.ess.factories.ScheduleFactory
-import com.avioconsulting.ess.models.Direction
+import com.avioconsulting.ess.models.MonthlySchedule
 import com.avioconsulting.ess.models.RecurringSchedule
 import org.joda.time.DateTimeZone
 import org.joda.time.LocalDate
 import org.joda.time.LocalTime
 
-import java.time.DayOfWeek
-
 class UpdatedScheduleFactory implements ScheduleFactory {
     RecurringSchedule createSchedule() {
-        def holidays = [
-                '2017-01-02',
-                '2017-02-20',
-                '2017-05-29',
-                '2017-07-04',
-                '2017-09-04',
-                '2017-11-23',
-                '2017-11-24'
-        ].collect { dateStr -> new LocalDate(dateStr) }
-
-        ScheduleBuilder.getWeeklySchedule name: 'TheSchedule',
-                                          displayName: 'the schedule2',
-                                          description: 'Weekly schedule on mondays',
-                                          startDate: new LocalDate(2017, 1, 2),
-                                          endDate: new LocalDate(2017, 11, 30),
-                                          timeOfDay: new LocalTime(9, 14, 10),
-                                          timeZone: DateTimeZone.forID('America/Chicago'),
-                                          daysOfWeek: [DayOfWeek.MONDAY],
-                                          holidays: holidays,
-                                          alternateDirection: Direction.Backward
+        new MonthlySchedule(name: 'the_sch_name',
+                            description: 'the description',
+                            displayName: 'the display name',
+                            timeZone: DateTimeZone.forID('America/Chicago'),
+                            startDate: new LocalDate(2025, 1, 1),
+                            endDate: new LocalDate(2025, 2, 1),
+                            repeatInterval: 2,
+                            daysOfMonth: [2, 12],
+                            timeOfDay: new LocalTime(9, 15, 10),
+                            includeDates: [],
+                            excludeDates: [])
     }
 }
