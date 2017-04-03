7e121d269638224ff299cb85273193bcc133401b
Brady Wied <brady@wied.us>
2017-04-03 10:44:56 -0600

    Explain why

--- src/main/groovy/com/avioconsulting/ess/mojos/FieldConstants.groovy
+++ src/main/groovy/com/avioconsulting/ess/mojos/FieldConstants.groovy
@@ -1,6 +1,7 @@
 package com.avioconsulting.ess.mojos
 
 class FieldConstants {
+    // we're remote and not within a module so we need the global JNDI namespace
     public static
     final String ESS_JNDI_EJB_METADATA = 'java:global.EssNativeHostingApp.native-ess-ejb.MetadataServiceBean!oracle.as.scheduler.MetadataServiceRemote'
     public static
